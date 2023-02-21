e5aba9958badd9ec498c7921d4cc82c4918cf763
luanferreira93 <luanf7321@gmail.com>
2023-02-21 14:06:24 -0300

    inital commit

---
+++ src/components/Tweet.tsx
@@ -0,0 +1,15 @@
+interface TweetProps{
+  text: string,
+  user: string,
+};
+
+
+export function Tweet({text,user}:TweetProps)
+{
+  return(
+    <div>
+      <h1>{text}</h1>
+      <small>{user}</small>
+    </div>
+  );
+};
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './global.css';
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <div className="layout">
+      <aside className="sidebar"></aside>
+      <div className="content">
+        
+      </div>
+    </div>
+  </React.StrictMode>,
+);

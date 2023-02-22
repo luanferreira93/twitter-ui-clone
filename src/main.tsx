437d4bd52efe5df6585332a6734d801026efe237
luanferreira93 <luanf7321@gmail.com>
2023-02-22 16:54:07 -0300

    Estruturação do sibebar e estilização

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Bell, BookBookmark, DotsThreeCircle, Envelope, FileText, Hash, House, User} from 'phosphor-react';
+import twitterlogo from './assets/logo-twitter.svg';
 import './global.css';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <div className="layout">
-      <aside className="sidebar"></aside>
+      <aside className="sidebar">
+        <img className="logo" src={twitterlogo} alt="logo do twitter" />
+        <nav className="main-navigation">
+            <a href="">
+              <House weight='fill' />
+              Home
+              </a>
+            <a href="">
+              <Hash/>
+              Explore
+              </a>
+            <a href="">
+              <Bell/>
+              Notifications
+              </a>
+            <a href="">
+              <Envelope/>
+              Messages
+              </a>
+            <a href="">
+              <BookBookmark/>
+              Bookmarks
+              </a>
+            <a href="">
+              <FileText/>
+              Lists
+              </a>
+            <a href="">
+              <User/>
+              Profile
+              </a>
+            <a href="">
+              <DotsThreeCircle/>
+              More
+              </a>
+        </nav>
+        <button className="new-tweet" type="button">Tweet</button>
+      </aside>
       <div className="content">
-        
+        content
       </div>
     </div>
   </React.StrictMode>,
